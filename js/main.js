1d68c997b0708420f246eb5cdb3e5ade88aff538
GiovaneBoaro <giovane.boaro@gmail.com>
2023-07-05 12:35:25 -0300

    finalizando evento do submit e função para criar elemento, salvando no localStore

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
 const form = document.getElementById('novoItem')
+const lista = document.getElementById("lista")
+const itens = []
+
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    evento.target[0].value
-    console.log("funcionando")
+    const nome =  evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
+    
+    nome.value = ""
+    quantidade.value = ""
+
 });
+
+function criaElemento(nome, quantidade) {
+    console.log(nome)
+    console.log(quantidade)
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem) 
+    novoItem.innerHTML += nome
+
+    lista.appendChild(novoItem)
+
+    const itemAtual = {
+        "nome":nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(itens))
+}
